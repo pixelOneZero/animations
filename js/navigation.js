761fb1cfa277865e7a864afa708ee890afa298a1
Michael McCarthy <miketmccarthy@gmail.com>
2015-02-01 17:38:16 -0800

    Add animated navigation

---
+++ js/navigation.js
@@ -0,0 +1,19 @@
+var poz = {
+	navigation: function() {
+		var container = document.getElementById("navigation");
+		var iconLines = document.getElementsByClassName("nav-icon");
+		container.addEventListener("click", function() {
+			for (var a=0; a <iconLines.length; a++) {
+				iconLines[a].classList.add("open");
+			}
+			container.classList.add("open");
+		});	
+	},
+	init: function() {
+		poz.navigation();
+	}
+}
+
+window.addEventListener("load", function() {
+	poz.init();
+})
